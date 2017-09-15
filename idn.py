3ea4f8e56e58447a10aa4737dafe160cf5ee1d0c
gokturkenez <hi@gokturkenez.com.tr>
2017-09-15 11:18:48 +0300

    Hash issue fixed with Param sorting.

---
+++ idn.py
@@ -0,0 +1,50 @@
+'''
+Project: PayU Turkey IDN API Python3 Sample Code
+Author: Göktürk Enez
+'''
+# Importing required libraries.
+from datetime import datetime
+import hmac
+import hashlib
+from urllib.parse import urlencode
+from urllib.request import Request, urlopen
+import collections
+
+#Endpoint
+url = 'https://secure.payu.com.tr/order/idn.php'
+
+#Secret Key
+secret = 'SECRET_KEY'
+
+array = collections.OrderedDict()
+array['MERCHANT'] = 'OPU_TEST'
+array['ORDER_REF'] = '39537992'
+array['ORDER_AMOUNT'] = '129.33'
+array['ORDER_CURRENCY'] = 'TRY'
+array['IDN_DATE'] = datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')
+array['CHARGE_AMOUNT'] = '129.33'
+
+# Initializing the hashstring @param
+hashstring = ''
+
+# Sorting Array @params
+for k, v in array.items():
+
+# Adding the UTF-8 byte length of each field value at the beginning of field value
+    hashstring += str(len(v.encode('UTF-8'))) + str(v)
+print(hashstring)
+
+# Calculating signature
+signature = hmac.new(secret.encode('utf-8'), hashstring.encode('utf-8'), hashlib.md5).hexdigest()
+
+# Adding signature @param to request array
+array['ORDER_HASH'] = signature
+print(signature)
+
+# Sending Request to Endpoint
+request = Request(url, urlencode(array).encode())
+response = urlopen(request).read().decode()
+
+# Printing result/response
+print(response)
+
